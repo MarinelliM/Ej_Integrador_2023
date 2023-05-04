f3c01c407a4b714e0e74d6dbb264d6d280b6d908
MarinelliM <103908022+MarinelliM@users.noreply.github.com>
2023-05-03 23:12:22 -0300

    Add files via upload

---
+++ Alumnos.py
@@ -0,0 +1,39 @@
+class Alumno:
+    __dni: int
+    __apellido: str
+    __nombre: str
+    __carrera: str
+    __año_carrera: int
+
+    def __init__(self, dni, apellido, nombre, carrera, año) -> None:
+        self.__dni = dni
+        self.__apellido = apellido
+        self.__nombre = nombre
+        self.__carrera = carrera
+        self.__año_carrera = año
+        pass
+
+    def __str__(self) -> str:
+        return "%8d %s %s %s %4d" % (self.__dni, self.__apellido, self.__nombre, self.__carrera, self.__año_carrera)
+        pass
+
+    def getdni(self):
+        return self.__dni
+    
+    def getañocar(self):
+        return self.__año_carrera
+    
+    def getapellido(self):
+         return self.__apellido
+    
+    def getnombre(self):
+         return self.__nombre
+    
+    def mostraralumno(self):
+        return print('Alumno: {} {}. Dni: {}. Carrera: {}. Año de cursado: {}' .format(self.__nombre,self.__apellido,self.__dni,self.__carrera,self.__año_carrera))
+    
+    def __lt__(self, otro):
+        if self.getañocar() == otro.getañocar():
+            return (self.getapellido(), self.getnombre()) < (otro.getapellido(), otro.getnombre())
+        else:
+            return self.getañocar() < otro.getañocar() and ((self.getapellido(), self.getnombre()) < (otro.getapellido(), otro.getnombre()))
